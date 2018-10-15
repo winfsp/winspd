5304fe394f7ae4fa3ca454bcf5db35727e088a78
Bill Zissimopoulos <billziss@navimatics.com>
2018-10-14 19:38:49 -0700

    project skeleton

---
+++ src/sys/driver.c
@@ -0,0 +1,37 @@
+/**
+ * @file sys/driver.c
+ *
+ * @copyright 2015-2018 Bill Zissimopoulos
+ */
+/*
+ * This file is part of WinSpd.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 3 as published by the Free Software
+ * Foundation.
+ *
+ * Licensees holding a valid commercial license may use this software
+ * in accordance with the commercial license agreement provided in
+ * conjunction with the software.  The terms and conditions of any such
+ * commercial license agreement shall govern, supersede, and render
+ * ineffective any application of the GPLv3 license to this software,
+ * notwithstanding of any reference thereto in the software or
+ * associated repository.
+ */
+
+#include <sys/driver.h>
+
+DRIVER_INITIALIZE DriverEntry;
+
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text(INIT, DriverEntry)
+#endif
+
+NTSTATUS DriverEntry(
+    PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    return STATUS_UNSUCCESSFUL;
+}
---
+++ src/sys/driver.h
@@ -0,0 +1,30 @@
+/**
+ * @file sys/driver.h
+ *
+ * @copyright 2018 Bill Zissimopoulos
+ */
+/*
+ * This file is part of WinSpd.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 3 as published by the Free Software
+ * Foundation.
+ *
+ * Licensees holding a valid commercial license may use this software
+ * in accordance with the commercial license agreement provided in
+ * conjunction with the software.  The terms and conditions of any such
+ * commercial license agreement shall govern, supersede, and render
+ * ineffective any application of the GPLv3 license to this software,
+ * notwithstanding of any reference thereto in the software or
+ * associated repository.
+ */
+
+#ifndef WINSPD_SYS_DRIVER_H_INCLUDED
+#define WINSPD_SYS_DRIVER_H_INCLUDED
+
+#define WINSPD_SYS_INTERNAL
+
+#define POOL_NX_OPTIN                   1
+#include <ntifs.h>
+
+#endif
