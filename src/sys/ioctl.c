564f2b225bc8fa682cf07abb46f307e1dc40c312
Bill Zissimopoulos <billziss@navimatics.com>
2018-11-05 13:13:43 -0800

    sys: ioctl: WIP

--- src/sys/ioctl.c
+++ src/sys/ioctl.c
@@ -21,14 +21,136 @@
 
 #include <sys/driver.h>
 
-VOID SpdHwProcessServiceRequest(PVOID DeviceExtension, PVOID Irp)
+static VOID SpdIoctlProvision(
+    ULONG InputBufferLength, ULONG ProcessId,
+    SPD_IOCTL_PROVISION_PARAMS *Params,
+    PIO_STATUS_BLOCK PIoStatus)
+{
+    ASSERT(PASSIVE_LEVEL == KeGetCurrentIrql());
+
+    if (sizeof *Params > InputBufferLength)
+    {
+        PIoStatus->Status = STATUS_INVALID_PARAMETER;
+        goto exit;
+    }
+
+    PIoStatus->Status = STATUS_INVALID_PARAMETER;
+
+exit:;
+}
+
+static VOID SpdIoctlUnprovision(
+    ULONG InputBufferLength, ULONG ProcessId,
+    SPD_IOCTL_UNPROVISION_PARAMS *Params,
+    PIO_STATUS_BLOCK PIoStatus)
+{
+    ASSERT(PASSIVE_LEVEL == KeGetCurrentIrql());
+
+    if (sizeof *Params > InputBufferLength)
+    {
+        PIoStatus->Status = STATUS_INVALID_PARAMETER;
+        goto exit;
+    }
+
+    PIoStatus->Status = STATUS_INVALID_PARAMETER;
+
+exit:;
+}
+
+static VOID SpdIoctlList(
+    ULONG InputBufferLength, ULONG OutputBufferLength,
+    SPD_IOCTL_LIST_PARAMS *Params,
+    PIO_STATUS_BLOCK PIoStatus)
+{
+    ASSERT(PASSIVE_LEVEL == KeGetCurrentIrql());
+
+    if (sizeof *Params > InputBufferLength)
+    {
+        PIoStatus->Status = STATUS_INVALID_PARAMETER;
+        goto exit;
+    }
+
+    PIoStatus->Status = STATUS_INVALID_PARAMETER;
+
+exit:;
+}
+
+static VOID SpdIoctlTransact(
+    ULONG InputBufferLength, ULONG OutputBufferLength, ULONG ProcessId,
+    SPD_IOCTL_TRANSACT_PARAMS *Params,
+    PIO_STATUS_BLOCK PIoStatus)
+{
+    ASSERT(PASSIVE_LEVEL == KeGetCurrentIrql());
+
+    if (sizeof *Params > InputBufferLength)
+    {
+        PIoStatus->Status = STATUS_INVALID_PARAMETER;
+        goto exit;
+    }
+
+    PIoStatus->Status = STATUS_INVALID_PARAMETER;
+
+exit:;
+}
+
+VOID SpdHwProcessServiceRequest(PVOID DeviceExtension, PVOID Irp0)
 {
     SPD_ENTER(ioctl,
         ASSERT(PASSIVE_LEVEL == KeGetCurrentIrql()));
 
+    PIRP Irp = Irp0;
+    PIO_STACK_LOCATION IrpSp = IoGetCurrentIrpStackLocation(Irp);
+    ULONG InputBufferLength = IrpSp->Parameters.DeviceIoControl.InputBufferLength;
+    ULONG OutputBufferLength = IrpSp->Parameters.DeviceIoControl.OutputBufferLength;
+    PVOID Params = Irp->AssociatedIrp.SystemBuffer;
+
+    if (sizeof(SPD_IOCTL_BASE_PARAMS) > InputBufferLength ||
+        ((SPD_IOCTL_BASE_PARAMS *)Params)->Size > InputBufferLength)
+    {
+        Irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+        goto exit;
+    }
+
+    switch (((SPD_IOCTL_BASE_PARAMS *)Params)->Code)
+    {
+    case SPD_IOCTL_PROVISION:
+        SpdIoctlProvision(InputBufferLength, IoGetRequestorProcessId(Irp),
+            Params, &Irp->IoStatus);
+        break;
+    case SPD_IOCTL_UNPROVISION:
+        SpdIoctlUnprovision(InputBufferLength, IoGetRequestorProcessId(Irp),
+            Params, &Irp->IoStatus);
+        break;
+    case SPD_IOCTL_PROVISION_PUBLIC:
+        SpdIoctlProvision(InputBufferLength, 0,
+            Params, &Irp->IoStatus);
+        break;
+    case SPD_IOCTL_UNPROVISION_PUBLIC:
+        SpdIoctlUnprovision(InputBufferLength, 0,
+            Params, &Irp->IoStatus);
+        break;
+    case SPD_IOCTL_LIST:
+        SpdIoctlList(InputBufferLength, OutputBufferLength,
+            Params, &Irp->IoStatus);
+        break;
+    case SPD_IOCTL_TRANSACT:
+        SpdIoctlTransact(InputBufferLength, OutputBufferLength, IoGetRequestorProcessId(Irp),
+            Params, &Irp->IoStatus);
+        break;
+    default:
+        Irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+        goto exit;
+    }
+
+exit:
+    if (STATUS_SUCCESS != Irp->IoStatus.Status &&
+        STATUS_BUFFER_OVERFLOW != Irp->IoStatus.Status)
+        Irp->IoStatus.Information = 0;
+    StorPortCompleteServiceIrp(DeviceExtension, Irp);
+
     SPD_LEAVE(ioctl,
         "%p, Irp=%p", "",
-        DeviceExtension, Irp);
+        DeviceExtension, Irp0);
 }
 
 VOID SpdHwCompleteServiceIrp(PVOID DeviceExtension)
