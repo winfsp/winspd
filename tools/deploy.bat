ca3c6d7fa5e10b23b5c7ea1fcfcaff0a26568794
Bill Zissimopoulos <billziss@navimatics.com>
2018-12-12 17:49:16 -0800

    build: sign catalog file

--- tools/deploy.bat
+++ tools/deploy.bat
@@ -10,7 +10,7 @@ set TARGET=\\%TARGET_MACHINE%%TARGET_ACCOUNT%
 
 cd %~dp0..
 mkdir %TARGET% 2>nul
-for %%f in (winspd-%SUFFIX%.inf winspd-%SUFFIX%.cat winspd-%SUFFIX%.sys winspd-%SUFFIX%.dll scsitool-%SUFFIX%.exe winspd-tests-%SUFFIX%.exe) do (
+for %%f in (winspd-%SUFFIX%.inf winspd-%SUFFIX%.cat winspd-%SUFFIX%.cer winspd-%SUFFIX%.sys winspd-%SUFFIX%.dll scsitool-%SUFFIX%.exe winspd-tests-%SUFFIX%.exe) do (
 	copy build\VStudio\build\%CONFIG%\%%f %TARGET% >nul
 )
 
@@ -21,5 +21,8 @@ for /f "tokens=2,*" %%i in ('reg query %RegKey% /v %RegVal% ^| findstr %RegVal%'
     set KitRoot="%%j"
 )
 copy %KitRoot%\Tools\%SUFFIX%\devcon.exe %TARGET% >nul
+copy %KitRoot%\bin\x86\certmgr.exe %TARGET% >nul
 
-echo devcon install winspd-%SUFFIX%.inf root\winspd >%TARGET%install.bat
+echo certmgr /add /c winspd-%SUFFIX%.cer /s /r localMachine root >%TARGET%install.bat
+echo certmgr /add /c winspd-%SUFFIX%.cer /s /r localMachine TrustedPublisher >>%TARGET%install.bat
+echo devcon install winspd-%SUFFIX%.inf root\winspd >>%TARGET%install.bat
--- tools/mkcat.bat
+++ tools/mkcat.bat
@@ -1,11 +1,19 @@
 @echo off
 
 setlocal
+setlocal EnableDelayedExpansion
 
 if "%1"=="x64" set OsVer=7_X64,8_X64,6_3_X64,10_X64,Server2008R2_X64,Server8_X64,Server6_3_X64,Server10_X64
 if "%1"=="x86" set OsVer=7_X86,8_X86,6_3_X86,10_X86
 if "%OsVer%"=="" goto usage
 shift
+
+if not "%1"=="-sign" goto skipsign
+shift
+set CertFile=%1
+shift
+if "%CertFile%"=="" goto usage
+:skipsign
 
 set BaseDir=%1
 if "%BaseDir%"=="" goto usage
@@ -28,6 +36,16 @@ if not "%1"=="" (
 echo inf2cat /driver:%TempDir% /os:%OsVer% /uselocaltime
 inf2cat /driver:%TempDir% /os:%OsVer% /uselocaltime
 if errorlevel 1 goto fail
+if not "%CertFile%"=="" (
+    for /F "delims=" %%l in ('certutil -dump "%CertFile%" ^| findstr /I /C:"Cert Hash(sha1)"') do (
+        for /F "tokens=2 delims=:" %%h in ("%%l") do (
+            set hash0=%%h
+            set hash=!hash0: =!
+        )
+    )
+    signtool sign /sha1 !hash! %TempDir%\*.cat
+    if errorlevel 1 goto fail
+)
 copy %TempDir%\*.cat . >nul
 if errorlevel 1 goto fail
 rmdir /s/q %TempDir%
@@ -38,5 +56,5 @@ rmdir /s/q %TempDir%
 exit /b 1
 
 :usage
-echo usage: mkcat x86^|x64 basedir files... 1>&2
+echo usage: mkcat x86^|x64 [-sign certfile.cer] basedir files... 1>&2
 exit /b 2
