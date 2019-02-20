e3911405a8c50af74213ddd8fb292bfd8603d85a
Bill Zissimopoulos <billziss@navimatics.com>
2019-02-20 14:42:17 -0800

    installer, devsetup: checkpoint

--- src/devsetup/devsetup.c
+++ src/devsetup/devsetup.c
@@ -20,48 +20,218 @@
  */
 
 #include <windows.h>
+#include <cfgmgr32.h>
+#include <newdev.h>
+#include <setupapi.h>
 #include <shared/minimal.h>
 
 #define PROGNAME                        L"devsetup"
 
-static void usage(void)
+/*
+ * Define MSITEST for MSI testing and no actual device setup.
+ */
+#define MSITEST
+
+static int MessageBoxFormat(DWORD Type, PWSTR Caption, PWSTR Format, ...)
 {
     WCHAR Buf[1024];
+    va_list ap;
 
-    wsprintfW(Buf, L""
+    va_start(ap, Format);
+    wvsprintfW(Buf, Format, ap);
+    va_end(ap);
+
+    return MessageBoxW(0, Buf, Caption, Type);
+}
+
+static void usage(void)
+{
+    MessageBoxFormat(MB_ICONEXCLAMATION | MB_OK, PROGNAME, L""
         "usage: %s COMMAND ARGS\n"
         "\n"
         "commands:\n"
-        "    add file.inf hwid\n"
+        "    add hwid file.inf\n"
         "    remove hwid\n",
         PROGNAME);
-
-    MessageBoxW(0, Buf, PROGNAME, MB_ICONEXCLAMATION | MB_OK);
 
     ExitProcess(ERROR_INVALID_PARAMETER);
 }
 
-static int add(PWSTR FileName, PWSTR HardwareId)
+#if !defined(MSITEST)
+
+static DWORD enumerate(PWSTR HardwareId, int (*Fn)(HDEVINFO DiHandle, SP_DEVINFO_DATA *Info))
 {
-    WCHAR Buf[1024];
+    HDEVINFO DiHandle = INVALID_HANDLE_VALUE;
+    SP_DEVINFO_DATA Info;
+    WCHAR PropBuf[1024];
+    BOOL Found = FALSE, RebootRequired = FALSE;
+    DWORD Error;
 
-    wsprintfW(Buf, L"add %s %s", FileName, HardwareId);
+    DiHandle = SetupDiGetClassDevsW(0, 0, 0, DIGCF_ALLCLASSES | DIGCF_PRESENT);
+    if (INVALID_HANDLE_VALUE == DiHandle)
+        goto lasterr;
 
-    MessageBoxW(0, Buf, PROGNAME, MB_OK);
+    Info.cbSize = sizeof Info;
+    for (DWORD I = 0; !Found && SetupDiEnumDeviceInfo(DiHandle, I, &Info); I++)
+    {
+        if (!SetupDiGetDeviceRegistryPropertyW(DiHandle, &Info, SPDRP_HARDWAREID, 0,
+            (PVOID)PropBuf, sizeof PropBuf - 2 * sizeof(WCHAR), 0))
+            continue;
+        PropBuf[sizeof PropBuf / 2 - 2] = L'\0';
+        PropBuf[sizeof PropBuf / 2 - 1] = L'\0';
 
-    return 0;
+        for (PWSTR P = PropBuf; L'\0' != *P; P = P + lstrlenW(P) + 1)
+            if (0 == invariant_wcsicmp(P, HardwareId))
+            {
+                if (0 != Fn)
+                {
+                    Error = Fn(DiHandle, &Info);
+                    if (0 != Error)
+                    {
+                        if (ERROR_SUCCESS_REBOOT_REQUIRED != Error)
+                            goto exit;
+
+                        RebootRequired = TRUE;
+                    }
+                }
+
+                Found = TRUE;
+                break;
+            }
+    }
+    if (!Found)
+        goto lasterr;
+
+    Error = RebootRequired ? ERROR_SUCCESS_REBOOT_REQUIRED : ERROR_SUCCESS;
+
+exit:
+    if (ERROR_NO_MORE_ITEMS == Error)
+        Error = ERROR_FILE_NOT_FOUND;
+
+    if (INVALID_HANDLE_VALUE != DiHandle)
+        SetupDiDestroyDeviceInfoList(DiHandle);
+
+    return Error;
+
+lasterr:
+    Error = GetLastError();
+    goto exit;
 }
 
-static int remove(PWSTR HardwareId)
+static DWORD add(PWSTR HardwareId, PWSTR FileName)
 {
-    WCHAR Buf[1024];
+    WCHAR FileNameBuf[MAX_PATH];
+    WCHAR HardwareIdBuf[LINE_LEN + 2];
+    WCHAR ClassName[MAX_CLASS_NAME_LEN];
+    GUID ClassGuid;
+    HDEVINFO DiHandle = INVALID_HANDLE_VALUE;
+    SP_DEVINFO_DATA Info;
+    SP_DEVINSTALL_PARAMS_W InstallParams;
+    BOOL RebootRequired0 = FALSE, RebootRequired1 = FALSE;
+    int Error;
 
-    wsprintfW(Buf, L"remove %s", HardwareId);
+    if (ERROR_SUCCESS != enumerate(HardwareId, 0))
+    {
+        if (0 == GetFullPathNameW(FileName, MAX_PATH, FileNameBuf, 0))
+            goto lasterr;
 
-    MessageBoxW(0, Buf, PROGNAME, MB_OK);
+        memset(HardwareIdBuf, 0, sizeof HardwareIdBuf);
+        lstrcpynW(HardwareIdBuf, HardwareId, LINE_LEN + 1);
 
-    return 0;
+        if (!SetupDiGetINFClassW(FileNameBuf, &ClassGuid, ClassName, MAX_CLASS_NAME_LEN, 0))
+            goto lasterr;
+
+        if (INVALID_HANDLE_VALUE == (DiHandle = SetupDiCreateDeviceInfoList(&ClassGuid, 0)))
+            goto lasterr;
+
+        Info.cbSize = sizeof Info;
+        if (!SetupDiCreateDeviceInfoW(DiHandle, ClassName, &ClassGuid, 0, 0, DICD_GENERATE_ID, &Info))
+            goto lasterr;
+
+        if (!SetupDiSetDeviceRegistryPropertyW(DiHandle, &Info,
+            SPDRP_HARDWAREID, (PVOID)HardwareIdBuf, (lstrlenW(HardwareIdBuf) + 2) * sizeof(WCHAR)))
+            goto lasterr;
+
+        if (!SetupDiCallClassInstaller(DIF_REGISTERDEVICE, DiHandle, &Info))
+            goto lasterr;
+
+        if (!SetupDiGetDeviceInstallParamsW(DiHandle, &Info, &InstallParams))
+            goto lasterr;
+        RebootRequired0 = 0 != (InstallParams.Flags & (DI_NEEDREBOOT | DI_NEEDRESTART));
+    }
+
+    if (!UpdateDriverForPlugAndPlayDevicesW(0, HardwareId, FileNameBuf, 0, &RebootRequired1))
+        goto lasterr;
+
+    Error = RebootRequired0 || RebootRequired1 ? ERROR_SUCCESS_REBOOT_REQUIRED : ERROR_SUCCESS;
+
+exit:
+    if (INVALID_HANDLE_VALUE != DiHandle)
+        SetupDiDestroyDeviceInfoList(DiHandle);
+
+    return Error;
+
+lasterr:
+    Error = GetLastError();
+    goto exit;
 }
+
+static DWORD removefn(HDEVINFO DiHandle, SP_DEVINFO_DATA *Info)
+{
+    SP_REMOVEDEVICE_PARAMS RemoveParams;
+    SP_DEVINSTALL_PARAMS_W InstallParams;
+    BOOL RebootRequired = FALSE;
+    DWORD Error;
+
+    RemoveParams.ClassInstallHeader.cbSize = sizeof RemoveParams.ClassInstallHeader;
+    RemoveParams.ClassInstallHeader.InstallFunction = DIF_REMOVE;
+    RemoveParams.Scope = DI_REMOVEDEVICE_GLOBAL;
+    RemoveParams.HwProfile = 0;
+    if (!SetupDiSetClassInstallParamsW(DiHandle, Info,
+        &RemoveParams.ClassInstallHeader, sizeof RemoveParams))
+        goto lasterr;
+
+    if (!SetupDiCallClassInstaller(DIF_REMOVE, DiHandle, Info))
+        goto lasterr;
+
+    if (!SetupDiGetDeviceInstallParamsW(DiHandle, Info, &InstallParams))
+        goto lasterr;
+    RebootRequired = 0 != (InstallParams.Flags & (DI_NEEDREBOOT | DI_NEEDRESTART));
+
+    Error = RebootRequired ? ERROR_SUCCESS_REBOOT_REQUIRED : ERROR_SUCCESS;
+
+exit:
+    return Error;
+
+lasterr:
+    Error = GetLastError();
+    goto exit;
+}
+
+static DWORD remove(PWSTR HardwareId)
+{
+    return enumerate(HardwareId, removefn);
+}
+
+#else
+
+static DWORD add(PWSTR HardwareId, PWSTR FileName)
+{
+    MessageBoxFormat(MB_ICONINFORMATION | MB_OK, PROGNAME,
+        L"add(HardwareId=%s, FileName=%s)", HardwareId, FileName);
+
+    return ERROR_SUCCESS;
+}
+
+static DWORD remove(PWSTR HardwareId)
+{
+    MessageBoxFormat(MB_ICONINFORMATION | MB_OK, PROGNAME,
+        L"remove(HardwareId=%s)", HardwareId);
+
+    return ERROR_SUCCESS;
+}
+
+#endif
 
 int wmain(int argc, wchar_t **argv)
 {
