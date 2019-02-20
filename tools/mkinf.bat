d2a7690752140e01e135f3f2b9666f590fccbfc1
Bill Zissimopoulos <billziss@navimatics.com>
2019-02-19 16:12:05 -0800

    tools: mkinf.bat

---
+++ tools/mkinf.bat
@@ -0,0 +1,42 @@
+@echo off
+
+setlocal
+setlocal EnableDelayedExpansion
+
+:varloop
+if [%1]==[] goto vardone
+if [%1]==[--] goto vardone
+set var=%1
+set var=!var::==!
+call set !var!
+shift
+goto varloop
+:vardone
+shift
+
+set src=%1
+set dst=%2
+if [!src!]==[] goto usage
+if [!dst!]==[] goto usage
+
+if exist !dst! del !dst!
+for /f "delims=" %%l in (!src!) do (
+    set line=%%l
+    echo !line! >>!dst!
+)
+
+set RegKey="HKLM\SOFTWARE\Microsoft\Windows Kits\Installed Roots"
+set RegVal="KitsRoot10"
+reg query %RegKey% /v %RegVal% >nul 2>&1 || (echo Cannot find Windows Kit >&2 & exit /b 1)
+for /f "tokens=2,*" %%i in ('reg query %RegKey% /v %RegVal% ^| findstr %RegVal%') do (
+    set KitRoot="%%j"
+)
+%KitRoot%\bin\x86\stampinf.exe -v !MyVersion! -f !dst!
+exit /b
+
+:fail
+exit /b 1
+
+:usage
+echo usage: mkinf variable:value... -- src dst 1>&2
+exit /b 2
